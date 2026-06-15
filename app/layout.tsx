caae63c3440297485d9ece54a1a9ff6bccaf8c41
Md Tahamedul Islam <tahamedulislam@gmail.com>
2026-06-15 15:51:19 +0600

    Add app/layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "NovaEdit",
+  description: "The modern writing platform",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className={inter.className}>
+      <body className="bg-background text-foreground antialiased">{children}</body>
+    </html>
+  );
+}
